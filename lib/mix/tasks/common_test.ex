822948c01cf7330c174e9c3cfe8a0c8626814f1a
Riccardo Manfrin <riccardo.manfrin@athonet.com>
2023-09-20 10:57:20 +0200

    Adds support for configs

--- lib/mix/tasks/common_test.ex
+++ lib/mix/tasks/common_test.ex
@@ -1,27 +1,47 @@
 defmodule Mix.Tasks.CommonTest do
+  require Logger
   use Mix.Task
 
   def run(args) do
 
-
     Mix.Task.run(:loadpaths)
 
-    ct_opts = CommonTest.Opts.parse(args)
+    test_spec_opts = CommonTest.Opts.parse(args)
 
-    paths = Keyword.get(ct_opts, :dirs)
-    suites = Keyword.get(ct_opts, :suites, ["*_suite"])
+    dirs = Keyword.get(test_spec_opts, :dirs)
+    suites = Keyword.get(test_spec_opts, :suites, ["*_suite"])
+    configs = Keyword.get(test_spec_opts, :config, [])
+    |> lookup_configs_from_suites(dirs, suites)
 
-    ct_opts = Keyword.drop(ct_opts, [:dirs, :suites])
+    test_spec_opts = Keyword.drop(test_spec_opts, [:dirs, :suites, :config])
     Application.put_env(:common_test, :auto_compile, false)
 
-    modules = lookup_modules_from_suites(paths, suites)
+    suites_modules = lookup_modules_from_suites(dirs, suites)
 
-    :ct.run_testspec([
-      {:suites, paths, modules} | ct_opts
-    ])
+    test_spec_opts = [{:suites, dirs, suites_modules} | test_spec_opts]
+
+    ## Doesn't work with config: []
+    test_spec_opts = case configs do
+      [] -> test_spec_opts
+      _ -> [{:config, configs} | test_spec_opts]
+    end
+
+    IO.inspect("Running testspec with opts #{inspect(test_spec_opts)}")
+
+    :ct.run_testspec(test_spec_opts)
 
   end
 
+  defp lookup_configs_from_suites([], paths, suites) do
+    for path <- paths do
+      for suite <- suites do
+        match = Path.join([path, "#{suite}_data", "#{suite}.config"])
+        Path.wildcard(match)
+      end
+    end |> List.flatten()
+    |> Enum.map(fn(f) -> String.to_charlist(f) end)
+  end
+  defp lookup_configs_from_suites(explicit_configs, _, _), do: explicit_configs
   defp lookup_modules_from_suites(paths, suites) do
     for path <- paths do
       for suite <- suites do
